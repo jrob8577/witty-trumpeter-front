44d14ff25edc1d5f4601b7b70b0b4b74bc71a92d
Devon Wesley <dwesley89@gmail.com>
2016-08-03 14:48:04 -0700

    react-router workeds

--- src/components/TheaterDetails.js
+++ src/components/TheaterDetails.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 import 'react-router'
 
+const API = 'http://localhost:4000'
 
 class TheaterDetails extends Component {
   componentDidMount() {
     this.setState({
-      theater: findUserById(this.props.params.theaterId)
+      theaterId: this.props.params.theaterId
     })
+
+    // fetch( `${API}/theater/${  }`, { mode: 'cors' } )
+    //   .then( response => response.json() )
+    //   .then( geocodeInfo => {
+    //     const center = (( geocodeInfo.results[ 0 ] || {} ).geometry || {} ).location || DEFAULT_CENTER
+    //
+    //     this.setState({ zipCode, theaters, center })
+    //   })
   }
 
   render() {
     return (
        <div>
-        <h2>{this.state.theater.theaterId}</h2>
+        <h2>Hello Buddy</h2>
       </div>
     )
   }
