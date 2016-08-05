6221ef9d30165893d599179aeb7a101191578038
Devon Wesley <dwesley89@gmail.com>
2016-08-05 11:44:46 -0700

    set remote for deploy

--- src/components/api.js
+++ src/components/api.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:4000'
-// export const API = 'https://immense-forest-73448.herokuapp.com'
+// export const API = 'http://localhost:4000'
+export const API = 'https://immense-forest-73448.herokuapp.com'
